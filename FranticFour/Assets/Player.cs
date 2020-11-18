3b52dc1efe48bdbfb50dad17cee6871680f7e138
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2020-11-18 11:33:20 +0100

    Non conflicting pushController  Pull

---
+++ FranticFour/Assets/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float pushForce = 10f;
+    [SerializeField] float pushCD = 2f;
+
+    PushController pushController;
+    MovementController movController;
+
+    bool canPush = true;
+
+    void Start()
+    {
+        pushController = GetComponentInChildren<PushController>();
+        movController = GetComponent<MovementController>();
+    }
+
+    private void Update()
+    {
+        if (canPush && (Input.GetButtonDown("Push") || Input.GetAxis("Push") > 0))
+        {
+            StartCoroutine(PushOtherPlayer());
+        }
+    }
+
+    IEnumerator PushOtherPlayer()
+    {
+        canPush = false;
+        pushController.PushTarget(movController.Dir.normalized * pushForce);
+        yield return new WaitForSeconds(pushCD);
+        canPush = true;
+    }
+}
--- FranticFour/Assets/PushController.cs
+++ FranticFour/Assets/PushController.cs
@@ -4,43 +4,38 @@ using UnityEngine;
 
 public class PushController : MonoBehaviour
 {
-    MovementController player;
-    [SerializeField] List<Transform> targets = new List<Transform>();
-
-
-    private void Start()
-    {
-        player = GetComponentInParent<MovementController>();
-    }
-
+    List<Transform> targets = new List<Transform>();
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if(other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player")
             targets.Add(other.gameObject.transform);
     }
 
     private void OnTriggerExit2D(Collider2D other)
     {
-        if(other.gameObject.tag == "Player")
+        if (other.gameObject.tag == "Player")
             targets.Remove(other.gameObject.transform);
     }
 
-    public MovementController GetTargetToPush()
+    public void PushTarget(Vector2 pushForce)
     {
-        if(targets.Count == 0)
-            return null;
+        if (targets.Count == 0)
+            return;
+        MovementController closestTarget = GetClosestTarget();
+        closestTarget.GetPushed(pushForce);
 
+    }
+
+    private MovementController GetClosestTarget()
+    {
         var closestTarget = targets[0];
-
         Transform player = gameObject.transform;
-
         float closestTargetDistance = Vector2.Distance(closestTarget.position, player.position);
 
         foreach (var target in targets)
         {
             float targetDistance = Vector2.Distance(target.position, player.position);
-
             if (targetDistance < closestTargetDistance)
             {
                 closestTarget = target;
