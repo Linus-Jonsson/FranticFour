c8a6156795525184a917023c0eb71c4de77404a1
RobinVondrak <robinvondrak01@gmail.com>
2020-11-21 00:04:21 +0100

    Player selection working. WIP
    Needs to transfer information to game scene.

--- FranticFour/Assets/01_Scripts/PlayerSelectionScene/InputManager.cs
+++ FranticFour/Assets/01_Scripts/PlayerSelectionScene/InputManager.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class InputManager : MonoBehaviour
 {
     [SerializeField] private int[] playerOwnedBy = new int[4];
     [SerializeField] private bool[] playersSelected = new bool[4];
+    [SerializeField] private TextMeshPro[] selectedText = new TextMeshPro[4]; 
 
     public void TakeNextPlayer(int controllerID)
     {
@@ -13,8 +16,10 @@ public class InputManager : MonoBehaviour
                 continue;
             
             playerOwnedBy[i] = controllerID;
+            selectedText[i].text = controllerID.ToString();
             playersSelected[i] = true;
             return;
         }
     }
+    //Ladda scener och sätt spelar till rätt
 }
