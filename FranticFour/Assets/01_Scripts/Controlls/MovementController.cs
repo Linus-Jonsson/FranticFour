9e1ce8763ad7f7854a52d405d498fc212a20e950
Linus-Jonsson <linus_jonsson@hotmail.com>
2020-11-20 11:49:00 +0100

    Fix + Prefab

--- FranticFour/Assets/01_Scripts/Controlls/MovementController.cs
+++ FranticFour/Assets/01_Scripts/Controlls/MovementController.cs
@@ -148,6 +148,7 @@ public class MovementController : MonoBehaviour
         StunBlink();
         yield return new WaitForSeconds(duration);
         freezeInput = false;
+        spriteRenderer.color = originalColor;
     }
 
     private async void StunBlink()
