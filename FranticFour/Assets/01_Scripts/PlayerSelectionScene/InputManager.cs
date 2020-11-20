6ad709e9c8cefd8ad3385f6ac650531931df5aa5
RobinVondrak <robinvondrak01@gmail.com>
2020-11-20 23:56:53 +0100

    TM Pro imported and Selection base

---
+++ FranticFour/Assets/01_Scripts/PlayerSelectionScene/InputManager.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private int[] playerOwnedBy = new int[4];
+    [SerializeField] private bool[] playersSelected = new bool[4];
+
+    public void TakeNextPlayer(int controllerID)
+    {
+        for (int i = 0; i < playerOwnedBy.Length; i++)
+        {
+            if (playersSelected[i] == true)
+                continue;
+            
+            playerOwnedBy[i] = controllerID;
+            playersSelected[i] = true;
+            return;
+        }
+    }
+}
