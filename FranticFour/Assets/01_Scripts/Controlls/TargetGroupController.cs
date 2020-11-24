dc3a1f1546b01bbcba3f05fa4a58b791c297db1c
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2020-11-24 15:24:41 +0100

    implemented some stuff
    implemented that our target group gets changed using code to always prioritize the prey
    
    removed some unused stuff
    
    changed color on the mixer groups.

---
+++ FranticFour/Assets/01_Scripts/Controlls/TargetGroupController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class TargetGroupController : MonoBehaviour
+{
+    [SerializeField] int preyWeight = 12;
+    [SerializeField] int hunterWeight = 6;
+    [SerializeField] float preyRadius = 1.5f;
+    [SerializeField] float hunterRadius = 1f;
+
+    CinemachineTargetGroup cinemachineGroup;
+
+    private void Start()
+    {
+        cinemachineGroup = FindObjectOfType<CinemachineTargetGroup>();
+    }
+
+    public void UpdateTargetGroup(Player[] players)
+    {
+        foreach (var player in players)
+        {
+            cinemachineGroup.RemoveMember(player.transform);
+
+            if (player.Prey)
+            {
+                cinemachineGroup.AddMember(player.transform, preyWeight, preyRadius);
+            }
+            else
+            {
+                cinemachineGroup.AddMember(player.transform, hunterWeight, hunterRadius);
+            }
+        }
+    }
+
+}
