7f4cb822b637187b0c025ae42fe5a1684d2e2215
RobinVondrak <robinvondrak01@gmail.com>
2020-11-27 10:40:44 +0100

    DET FUNKAR!!!!!!!!!!!!!!

--- FranticFour/Assets/01_Scripts/PlayerSelectionScene/SelectionController.cs
+++ FranticFour/Assets/01_Scripts/PlayerSelectionScene/SelectionController.cs
@@ -1,21 +1,28 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class SelectionController : MonoBehaviour
 {
-    [Header("Player")] 
-    [Range(0, 3)] [SerializeField] private int CONTROLLER_ID = 0;
+    [Header("Player")] [Range(0, 3)] [SerializeField]
+    private int CONTROLLER_ID = 0;
+
+    private int selected;
+    private float inputZone = 0.04f;
+    private bool inputBool = false;
 
     [SerializeField] private bool controllerAssignedToPlayer = false;
 
-    [Header("Assigned controls")]
-    [SerializeField] private string action1;
+    [Header("Assigned controls")] [SerializeField]
+    private string action1;
+
+    private string rightHorizontal;
+    private string rightVertical;
 
     [SerializeField] private InputManager playerHandler;
-    
-    [SerializeField]
 
     private void Start()
     {
+        selected = CONTROLLER_ID;
         playerHandler = FindObjectOfType<InputManager>();
 
         string controllerName = "";
@@ -30,17 +37,27 @@ public class SelectionController : MonoBehaviour
             controllerName = controllersConnected[CONTROLLER_ID];
 
         if (controllerName.Contains("xbox"))
+        {
             action1 = StringManager.Inputs.action1 + CONTROLLER_ID;
+            rightHorizontal = StringManager.Inputs.rightHorizontal + CONTROLLER_ID;
+            rightVertical = StringManager.Inputs.rightVertical + CONTROLLER_ID;
+        }
         else if (controllerName.Contains("wireless controller"))
+        {
             action1 = StringManager.Inputs.action1PS4 + CONTROLLER_ID;
+            rightHorizontal = StringManager.Inputs.rightHorizontalPS4 + CONTROLLER_ID;
+            rightVertical = StringManager.Inputs.rightVerticalPS4 + CONTROLLER_ID;
+        }
         else
         {
+            Destroy(gameObject); //DEBUG
             if (!PassControllersToGame.isKeyboardUsed)
             {
                 PassControllersToGame.isKeyboardUsed = true;
                 PassControllersToGame.keyBoardOwnedBy = CONTROLLER_ID;
                 action1 = StringManager.Inputs.action1Keyboard;
-                Debug.LogWarning("Controller is missing or cant be recognized: " + controllerName + " You are set to keyboard");
+                Debug.LogWarning("Controller is missing or cant be recognized: " + controllerName +
+                                 " You are set to keyboard");
             }
             else
             {
@@ -63,6 +80,29 @@ public class SelectionController : MonoBehaviour
             controllerAssignedToPlayer = true;
             Debug.Log("Player assigned" + Input.GetButton(action1));
             playerHandler.TakeNextPlayer(CONTROLLER_ID);
+        }
+
+        float m_inputX = Input.GetAxis(rightHorizontal);
+        float m_inputY = Input.GetAxis(rightVertical);
+
+        if (m_inputX > inputZone || m_inputX < -inputZone || m_inputY < -inputZone || m_inputY > inputZone)
+        {
+            if (inputBool)
+                return;
+
+            if (m_inputX > inputZone) m_inputX = Mathf.CeilToInt(m_inputX);
+            else if (m_inputX < -inputZone) m_inputX = Mathf.FloorToInt(m_inputX);
+
+            if (m_inputY > inputZone) m_inputY = Mathf.CeilToInt(m_inputY);
+            else if (m_inputY < -inputZone) m_inputY = Mathf.FloorToInt(m_inputY);
+
+            inputBool = true;
+            Debug.LogWarning(m_inputX);
+            playerHandler.SelectNext(CONTROLLER_ID, ref selected, (int)m_inputX, (int)m_inputY);
+        }
+        else
+        {
+            inputBool = false;
         }
     }
 }
