dcd26e85a638feceb847535e0c165fa195c92aeb
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2020-12-02 11:05:21 +0100

    Fixed a way to be able to jump over holes
    now linus need to connect it properly
    
    also fed all the bugs.

--- FranticFour/Assets/01_Scripts/Player.cs
+++ FranticFour/Assets/01_Scripts/Player.cs
@@ -34,6 +34,14 @@ public class Player : MonoBehaviour
     Player pushedBy = null;
     public Player PushedBy { get { return pushedBy; } set { pushedBy = value; } }
 
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.layer == 13)
+            print("Triggered a hole and is now falling to death");
+        else
+            print("Jumped over the hole");
+    }
+
     private void Awake()
     {
         playerNumber = GetComponent<AssignedController>().PlayerID + 1;        
