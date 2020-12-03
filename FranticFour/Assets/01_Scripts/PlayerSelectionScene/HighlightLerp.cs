c94367dd21ee536cb103ecd777c3bb4b20d1101a
Linus-Jonsson <linus_jonsson@hotmail.com>
2020-12-03 01:17:20 +0100

    Revert "Revert "Merge branch 'main' into Garden---Assets-Update""
    This reverts commit 44c05d90dc3f304d316cde0d3ba66f382ba4c1ab.

---
+++ FranticFour/Assets/01_Scripts/PlayerSelectionScene/HighlightLerp.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using UnityEngine;
+
+public class HighlightLerp : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer spriteRenderer;
+    [SerializeField] private Color originalColor;
+    [SerializeField] private float originalAlpha;
+    [SerializeField] private float lerpSpeed;
+    [SerializeField] public bool isLerping;
+    [SerializeField] public bool hasSelected;
+
+    private void Awake()
+    {
+        originalColor = spriteRenderer.color;
+        originalAlpha = originalColor.a;
+    }
+
+    public void StartLerp()
+    {
+        spriteRenderer.color = new Color(
+            originalColor.r,
+            originalColor.g,
+            originalColor.b,
+            originalColor.a = 0);
+        isLerping = true;
+    }
+
+    private void Update()
+    {
+        if (isLerping)
+            LerpAlpha();
+        if (hasSelected)
+            LerpSelect();
+    }
+
+    private void LerpSelect()
+    {
+        //Todo Make transition better
+        if (originalColor.a >= 1f)
+            hasSelected = false;
+
+        Color.RGBToHSV(spriteRenderer.color, out float _h, out float _s, out float _v);
+        _s += Time.fixedTime * 0.01f;
+        Color m_temp = Color.HSVToRGB(_h, _s, _v);
+        
+        m_temp = new Color(
+            originalColor.r,
+            originalColor.g,
+            originalColor.b,
+            originalColor.a += Time.fixedTime * 0.01f);
+
+        spriteRenderer.color = m_temp;
+    }
+
+    private void LerpAlpha()
+    {
+        spriteRenderer.color = new Color (
+            originalColor.r,
+            originalColor.g,
+            originalColor.b, 
+            Mathf.PingPong(Time.fixedTime * lerpSpeed, originalAlpha));
+    }
+}
---
+++ FranticFour/Assets/01_Scripts/PlayerSelectionScene/MyPlayer.cs
@@ -0,0 +1,31 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class MyPlayer : MonoBehaviour
+{
+    [Header("Visual stuff for player")]
+    [Range(1,4)]
+    [SerializeField] private int playerID;
+    [SerializeField] private GameObject pressToJoinGameObject;
+    [SerializeField] private TextMeshPro pressToJoinTmPro;
+    [SerializeField] private GameObject pressToJoinIcon;
+    [SerializeField] private SpriteRenderer pressToJoinSpriteRenderer;
+    [SerializeField] private GameObject selectedHighlight;
+    [SerializeField] private Color selectedColor;
+    [SerializeField] private int selectedColorAlpha = 70;
+    [SerializeField] private HighlightLerp selectedHighlightLerp;
+    [SerializeField] private CharacterSelection selectedCharacterSelection;
+    [SerializeField] private GameObject characterSelectionGameObject;
+
+    public int PlayerID => playerID;
+    public GameObject PressToJoinGameObject => pressToJoinGameObject;
+    public TextMeshPro PressToJoinTmPro => pressToJoinTmPro;
+    public GameObject PressToJoinIcon => pressToJoinIcon;
+    public SpriteRenderer PressToJoinSpriteRenderer => pressToJoinSpriteRenderer;
+    public GameObject SelectedHighlight => selectedHighlight;
+    public Color SelectedColor => selectedColor;
+    public int SelectedColorAlpha => selectedColorAlpha;
+    public HighlightLerp SelectedHighlightLerp => selectedHighlightLerp;
+    public CharacterSelection SelectedCharacterSelection => selectedCharacterSelection;
+    public GameObject CharacterSelectionGameObject => characterSelectionGameObject;
+}
