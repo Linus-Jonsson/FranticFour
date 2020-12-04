860b543bfc4d2abf99740bfdfe88ee60e3d94cec
RobinVondrak <robinvondrak01@gmail.com>
2020-12-04 14:36:40 +0100

    NEED THIS

--- FranticFour/Assets/01_Scripts/HunterRespawnHandler.cs
+++ FranticFour/Assets/01_Scripts/HunterRespawnHandler.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 [ExecuteInEditMode]
@@ -10,23 +12,34 @@ public class HunterRespawnHandler : MonoBehaviour
     [SerializeField] float maxDistance = 20f;
 
     [SerializeField] Transform prey = null; // remove serializeField once all works
-     
+    [SerializeField] private bool trySpawn = false;
+
     private void Start()
     {
         activeSpawnPoints = GetComponentsInChildren<Transform>();
     }
 
+    private void SHitFuckFuck()
+    {
+        if (trySpawn) GetSpawnPoint();
+    }
+
     public Transform GetSpawnPoint()
     {
+        trySpawn = true;
+        if (prey is null)
+            SHitFuckFuck();
+        trySpawn = false;
+
         foreach (var transform in activeSpawnPoints)
         {
             float xDistance = Mathf.Abs(transform.position.x - prey.position.x);
             float yDistance = Mathf.Abs(transform.position.y - prey.position.y);
-            if(xDistance > minDistance && xDistance < maxDistance && yDistance > minDistance && yDistance < maxDistance)
-            {
+            if (xDistance > minDistance && xDistance < maxDistance && yDistance > minDistance &&
+                yDistance < maxDistance)
                 return transform;
-            }
         }
+
         return activeSpawnPoints[0];
     }
 
