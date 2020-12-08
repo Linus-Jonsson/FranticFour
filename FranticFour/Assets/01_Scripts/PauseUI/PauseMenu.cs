1df6e2ef972194dbb45bea914a59e66daa60f4f2
RobinVondrak <robinvondrak01@gmail.com>
2020-12-08 15:41:28 +0100

    Ding dong, i need food

--- FranticFour/Assets/01_Scripts/PauseUI/PauseMenu.cs
+++ FranticFour/Assets/01_Scripts/PauseUI/PauseMenu.cs
@@ -1,18 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
 public class PauseMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    //0 = button | 1 = place to move to | 2 = gameobject to move | 3 = place to move to
+    [SerializeField] public GameObject[] buttons = new GameObject[5];
 
-    // Update is called once per frame
-    void Update()
+    public void MoveToKeybindsFromMain()
     {
-        
+        LeanTween.moveX(buttons[0], buttons[1].transform.position.x, 1f);
+        LeanTween.moveX(buttons[2], buttons[3].transform.position.x, 1f);
+    }
+    
+    public void MoveBackToMainFromKeybinds()
+    {
+        LeanTween.moveX(buttons[0], buttons[3].transform.position.x, 1f);
+        LeanTween.moveX(buttons[2], buttons[4].transform.position.x, 1f);
     }
 }
