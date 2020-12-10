5ce9c642e1928dff5d4e0b23127310f20b1636f1
RobinVondrak <robinvondrak01@gmail.com>
2020-12-10 09:57:22 +0100

    Display controller relative to player

---
+++ FranticFour/Assets/01_Scripts/PlayerSelectionScene/ControllersDisplay.cs
@@ -0,0 +1,24 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class ControllersDisplay : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro[] displayText = new TextMeshPro[4];
+    private string[] controllers = null;
+    private bool isKeyboardUsed = false;
+
+    private void Update()
+    {
+        controllers = Input.GetJoystickNames();
+
+        if (controllers.Length < 4) isKeyboardUsed = true;
+
+        for (int  i = 0; i <= controllers.Length; i++)
+        {
+            if (i == controllers.Length && isKeyboardUsed)
+                displayText[i].text = $"ID{i}: Keyboard";
+            else if (i != controllers.Length) 
+                displayText[i].text = $"ID{i}: {controllers[i]}";
+        }
+    }
+}
