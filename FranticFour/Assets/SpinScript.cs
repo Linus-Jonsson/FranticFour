92179f246108c86d0c2183e215fbda7312006a92
NicklasHidesjo <Nicklas.hidesjo@outlook.com>
2020-12-17 16:24:31 +0100

    UWU Malva merge UWU

---
+++ FranticFour/Assets/SpinScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinScript : MonoBehaviour
+{
+    [SerializeField] float rotationSpeed = 10f;
+
+    Vector3 rotation = new Vector3(0,0,0);
+
+    void Update()
+    {
+        rotation += new Vector3(0, 0, rotationSpeed * Time.deltaTime);
+        gameObject.transform.rotation = Quaternion.Euler(rotation);
+    }
+}
